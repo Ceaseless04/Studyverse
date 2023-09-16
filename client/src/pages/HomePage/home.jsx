28139f30104bb699d63cb8906fb49a694268da87
catastrxphic <mcopo001@fiu.edu>
2023-09-16 18:02:19 -0400

    added template for develop of homescreen

---
+++ client/src/pages/HomePage/home.jsx
@@ -0,0 +1,15 @@
+import "./home.css";
+// when you are doing import of components (navbar, etc) follow this format 
+// import Function from "../../components/function";
+
+
+function Home() {
+    return (
+        <div className="home-page">
+
+            <p>This is like HTML</p>
+
+        </div>
+    );
+}
+export default Home;
