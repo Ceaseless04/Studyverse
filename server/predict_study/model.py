0ec773edaf6147e8463b4bb23c29e24cbee7da2f
catastrxphic <mcopo001@fiu.edu>
2023-09-16 19:40:42 -0400

    ML

--- server/predict_study/model.py
+++ server/predict_study/model.py
@@ -12,9 +12,9 @@ dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.cs
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
 
-x_train, x_test, y_train, y_test = dftrain(dftrain.drop['survived'], dftrain['survived'],test_size=0.8)
+x_train, x_test, y_train, y_test = dftrain(dftrain.drop('survived'), dftrain['survived'],test_size=0.8)
 
 #number of trees you will be using to train model. tune!!
 model = RandomForestClassifier(n_estimators=10)
 model.fit(x_train, y_train) # training the model
-model.score(x_test, y_test)
+model.score(x_test, y_test) 
