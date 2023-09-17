c5a12559d5b3757d8e93a31621715268d50ad186
Ceaseless04 <kristian120304@gmail.com>
2023-09-16 23:50:55 -0400

    Got data set, put it in mongo, now need to use APIs to communicate the whole project together

---
+++ client/src/app/pages/Login/Login.jsx
@@ -0,0 +1,5 @@
+export function Login() {
+    return (
+        <input placeholder="please enter input"></input>
+    );
+}
---
+++ client/src/app/pages/Login/page.js
@@ -0,0 +1,10 @@
+import { Login } from "./Login";
+
+export default function Hi() {
+    return (
+        <>
+        <h1>Doing stuff</h1>
+        <Login />
+        </>
+    );
+}
