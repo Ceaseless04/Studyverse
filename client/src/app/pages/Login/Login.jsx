7d556b48b573baa417a1a5ff6d87bd61fa1d93a6
catastrxphic <mcopo001@fiu.edu>
2023-09-17 00:02:58 -0400

    draft style

--- client/src/app/pages/Login/Login.jsx
+++ client/src/app/pages/Login/Login.jsx
@@ -5,6 +5,19 @@ import "./Login.css";
 
 export function Login() {
     return (
-        <h1>Login page</h1>
+        <>
+            <h1>Login page</h1>
+
+            <div className="container">
+                <label> Email </label> <br/>
+                <input type="email" class="email" placeholder="jkRowl@gmail.com" required/> 
+                        
+            </div>
+
+            <button type="submit"> log in</button>
+
+        </>
+       
+
     );
 }
--- client/src/app/pages/Signup/Signup.jsx
+++ client/src/app/pages/Signup/Signup.jsx
@@ -5,6 +5,26 @@ import "./Signup.css";
 
 export function Signup() {
     return (
-        <h1>Signup page</h1>
+        <>
+        <div className="MaxContainer">
+            <h1 className="tagName">Signup page</h1>
+
+            <div className="container1 containers">
+                <label className="inputLabel"> Email</label>
+                <input type="email" class="inputEmail" placeholder="jkRowl@gmail.com" required/> 
+                <br/><br/>
+            </div>
+
+            <div className="container2 containers">
+                <label className="inputLabel"> Username </label>
+                <br/>
+                <input type="username" class="inputUserN" placeholder="jKRow33" required/> 
+                <br/><br/>
+            </div>
+            
+            <button type="submit" className="SubButton"> Verify email</button>
+
+        </div>    
+        </>
     );
 }
