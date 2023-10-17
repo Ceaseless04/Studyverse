27119a1f517e1e1297037f0022daddaaaa445b44
Ceaseless04 <kristian120304@gmail.com>
2023-10-17 19:05:55 -0400

    need to troubleshoot at home :(

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\krist\Downloads\coding\ShellHacks_2023\Studyverse\server\venv
+set VIRTUAL_ENV=C:\Users\krist\Downloads\Studyverse\Studyverse\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
